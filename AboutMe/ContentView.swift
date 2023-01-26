ed03f7e5ac59af63c930267c49b6a55a323524f8
Pranav <pthomas76@gatech.edu>
2023-01-26 11:31:47 -0500

    First commit

--- AboutMe/ContentView.swift
+++ AboutMe/ContentView.swift
@@ -9,13 +9,28 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        TabView {
+            HomeView()
+                .tabItem {
+                    Label("Home", systemImage: "person")
+                }
+
+            StoryView()
+                .tabItem {
+                    Label("Story", systemImage: "book")
+                }
+
+            FavoritesView()
+                .tabItem {
+                    Label("Favorites", systemImage: "star")
+                }
+
+            FunFactsView()
+                .tabItem {
+                    Label("Fun Facts", systemImage: "hand.thumbsup")
+                }
         }
-        .padding()
+        
     }
 }
 
---
+++ AboutMe/StoryView.swift
@@ -0,0 +1,32 @@
+//
+//  StoryView.swift
+//  AboutMe
+//
+//  Created by Pranav Thomas on 1/26/23.
+//
+
+import SwiftUI
+
+struct StoryView: View {
+    var body: some View {
+        VStack {
+            Text("My Story")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .padding()
+            
+            ScrollView {
+                Text(information.story)
+                    .font(.body)
+                    .padding()
+            }
+        }
+        .padding([.top, .bottom], 50)
+    }
+}
+
+struct StoryView_Previews: PreviewProvider {
+    static var previews: some View {
+        StoryView()
+    }
+}
